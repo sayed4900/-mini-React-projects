6a95e93f7985ae863eb34ba58d086181e81061f2
Sayed Abdo <Sayedabdo4900@gmail.com>
2023-06-24 15:01:05 +0300

    finish first project

---
+++ project-one/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Navbar(){
+  return (
+    <header>
+      <ul>
+        <li><img src="/react-logo.png" className="logo"/></li>
+        <li> <h1>React Facts</h1></li>
+      </ul>
+      <div>
+        <h3>React Course - Project 1</h3>
+      </div>
+    </header>
+  )
+}
